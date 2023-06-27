d1473e7f5ab0dbd8a3e8db18fb9080dbf2fee8ba
Martial Anouman <martialanouman@arolitec.com>
2023-06-27 17:30:48 +0000

    chore: add and setup remix-flat-route dep

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,20 @@
+const { flatRoutes } = require('remix-flat-routes')
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  future: {
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
-  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
-  postcss: true,
-  serverModuleFormat: "cjs",
-  tailwind: true,
-};
+	cacheDirectory: './node_modules/.cache/remix',
+	future: {
+		v2_errorBoundary: true,
+		v2_headers: true,
+		v2_meta: true,
+		v2_normalizeFormMethod: true,
+		v2_routeConvention: true,
+	},
+	ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
+	routes: async defineRoutes => {
+		flatRoutes('routes', defineRoutes)
+	},
+	postcss: true,
+	serverModuleFormat: 'cjs',
+	tailwind: true,
+}
