dfad21ce3af7ffd9043933ed5beb7e87c38b3754
Martial Anouman <martialanouman@arolitec.com>
2023-06-29 13:32:57 +0000

    refactor: update home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react'
 
 import { useOptionalUser } from '~/utils'
 
-export const meta: V2_MetaFunction = () => [{ title: 'Remix Notes' }]
+export const meta: V2_MetaFunction = () => [{ title: 'Nobu Stack' }]
 
 export default function Index() {
 	const user = useOptionalUser()
@@ -23,7 +23,7 @@ export default function Index() {
 						<div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
 							<h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
 								<span className="block uppercase text-blue-500 drop-shadow-md">
-									Blues Stack
+									Nobu Stack
 								</span>
 							</h1>
 							<p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
@@ -32,12 +32,7 @@ export default function Index() {
 							</p>
 							<div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
 								{user ? (
-									<Link
-										to="/notes"
-										className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-									>
-										View Notes for {user.email}
-									</Link>
+									<div className="text-lg">Welcome {user?.email}!</div>
 								) : (
 									<div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
 										<Link
