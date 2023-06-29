4df14dc813ee6a53160aa5025cdc58431d3cedd1
Martial Anouman <martialanouman@arolitec.com>
2023-06-29 14:34:55 +0000

    style: setup daisyui
    set tailwind plugin
    link base styles
    move tailwind style to app/styles folder
    move daisyui to dev deps

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,44 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction, LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from '@remix-run/react'
 
-import { getUser } from "~/session.server";
-import stylesheet from "~/tailwind.css";
+import { getUser } from '~/session.server'
+import tailwindStylesHref from './styles/tailwind.css?inline'
+import appStylesHref from './styles/app.css?inline'
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+	{ rel: 'stylesheet', href: tailwindStylesHref },
+	{ rel: 'stylesheet', href: appStylesHref },
+	...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+]
 
 export const loader = async ({ request }: LoaderArgs) => {
-  return json({ user: await getUser(request) });
-};
+	return json({ user: await getUser(request) })
+}
 
 export default function App() {
-  return (
-    <html lang="en" className="h-full">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="h-full">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="h-full">
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
