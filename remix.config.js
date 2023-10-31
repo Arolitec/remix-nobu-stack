80c21392db0223f006c4e5e8d89c8622a2402c73
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2023-10-31 17:57:33 +0000

    chore: wip

--- remix.config.js
+++ remix.config.js
@@ -3,14 +3,6 @@ const { flatRoutes } = require('remix-flat-routes')
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	cacheDirectory: './node_modules/.cache/remix',
-	future: {
-		v2_errorBoundary: true,
-		v2_headers: true,
-		v2_meta: true,
-		v2_normalizeFormMethod: true,
-		v2_routeConvention: true,
-		v2_dev: true,
-	},
 	ignoredRouteFiles: ['**/*'],
 	routes: async defineRoutes => {
 		return flatRoutes('routes', defineRoutes)
