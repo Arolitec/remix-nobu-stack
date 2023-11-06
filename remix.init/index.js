bd053659de3f71773b32e8dc6646776a3cf46cd8
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2023-11-06 10:28:03 +0000

    refactor: Refactor code to improve performance and readability

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,222 +1,218 @@
-const {execSync} = require("child_process")
-const crypto = require("crypto")
-const fs = require("fs/promises")
-const path = require("path")
+const { execSync } = require('child_process')
+const crypto = require('crypto')
+const fs = require('fs/promises')
+const path = require('path')
 
-const PackageJson = require("@npmcli/package-json")
-const semver = require("semver")
-const YAML = require("yaml")
+const PackageJson = require('@npmcli/package-json')
+const semver = require('semver')
+const YAML = require('yaml')
 
-const cleanupCypressFiles = ({fileEntries, isTypeScript, packageManager}) =>
-    fileEntries.flatMap(([filePath, content]) => {
-        let newContent = content.replace(
-            new RegExp("npx ts-node", "g"),
-            isTypeScript ? `${packageManager.exec} ts-node` : "node"
-        )
+const cleanupCypressFiles = ({ fileEntries, isTypeScript, packageManager }) =>
+	fileEntries.flatMap(([filePath, content]) => {
+		let newContent = content.replace(
+			/npx ts-node/g,
+			isTypeScript ? `${packageManager.exec} ts-node` : 'node',
+		)
 
-        if (!isTypeScript) {
-            newContent = newContent
-                .replace(new RegExp("create-user.ts", "g"), "create-user.js")
-                .replace(new RegExp("delete-user.ts", "g"), "delete-user.js")
-                .replace(new RegExp("create-verification.ts", "g"), "create-verification.js")
-        }
+		if (!isTypeScript) {
+			newContent = newContent
+				.replace(/create-user.ts/g, 'create-user.js')
+				.replace(/delete-user.ts/g, 'delete-user.js')
+				.replace(/create-verification.ts/g, 'create-verification.js')
+		}
 
-        return [fs.writeFile(filePath, newContent)]
-    })
+		return [fs.writeFile(filePath, newContent)]
+	})
 
 const cleanupDeployWorkflow = (deployWorkflow, deployWorkflowPath) => {
-    delete deployWorkflow.jobs.typecheck
-    deployWorkflow.jobs.deploy.needs = deployWorkflow.jobs.deploy.needs.filter(
-        (need) => need !== "typecheck"
-    )
+	delete deployWorkflow.jobs.typecheck
+	deployWorkflow.jobs.deploy.needs = deployWorkflow.jobs.deploy.needs.filter(
+		need => need !== 'typecheck',
+	)
 
-    return [fs.writeFile(deployWorkflowPath, YAML.stringify(deployWorkflow))]
+	return [fs.writeFile(deployWorkflowPath, YAML.stringify(deployWorkflow))]
 }
 
 const cleanupVitestConfig = (vitestConfig, vitestConfigPath) => {
-    const newVitestConfig = vitestConfig.replace(
-        "setup-test-env.ts",
-        "setup-test-env.js"
-    )
+	const newVitestConfig = vitestConfig.replace(
+		'setup-test-env.ts',
+		'setup-test-env.js',
+	)
 
-    return [fs.writeFile(vitestConfigPath, newVitestConfig)]
+	return [fs.writeFile(vitestConfigPath, newVitestConfig)]
 }
 
-const escapeRegExp = (string) =>
-    // $& means the whole matched string
-    string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+const escapeRegExp = string =>
+	// $& means the whole matched string
+	string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
-const getPackageManagerCommand = (packageManager) =>
-    // Inspired by https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L38-L103
-    ({
-        npm: () => ({
-            exec: "npx",
-            lockfile: "package-lock.json",
-            run: (script, args) => `npm run ${script} ${args ? `-- ${args}` : ""}`,
-        }),
-        pnpm: () => {
-            const pnpmVersion = getPackageManagerVersion("pnpm")
-            const includeDoubleDashBeforeArgs = semver.lt(pnpmVersion, "7.0.0")
-            const useExec = semver.gte(pnpmVersion, "6.13.0")
+const getPackageManagerCommand = packageManager =>
+	// Inspired by https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L38-L103
+	({
+		npm: () => ({
+			exec: 'npx',
+			lockfile: 'package-lock.json',
+			run: (script, args) => `npm run ${script} ${args ? `-- ${args}` : ''}`,
+		}),
+		pnpm: () => {
+			const pnpmVersion = getPackageManagerVersion('pnpm')
+			const includeDoubleDashBeforeArgs = semver.lt(pnpmVersion, '7.0.0')
+			const useExec = semver.gte(pnpmVersion, '6.13.0')
 
-            return {
-                exec: useExec ? "pnpm exec" : "pnpx",
-                lockfile: "pnpm-lock.yaml",
-                run: (script, args) =>
-                    includeDoubleDashBeforeArgs
-                        ? `pnpm run ${script} ${args ? `-- ${args}` : ""}`
-                        : `pnpm run ${script} ${args || ""}`,
-            }
-        },
-        yarn: () => ({
-            exec: "yarn",
-            lockfile: "yarn.lock",
-            run: (script, args) => `yarn ${script} ${args || ""}`,
-        }),
-    }[packageManager]())
+			return {
+				exec: useExec ? 'pnpm exec' : 'pnpx',
+				lockfile: 'pnpm-lock.yaml',
+				run: (script, args) =>
+					includeDoubleDashBeforeArgs
+						? `pnpm run ${script} ${args ? `-- ${args}` : ''}`
+						: `pnpm run ${script} ${args || ''}`,
+			}
+		},
+		yarn: () => ({
+			exec: 'yarn',
+			lockfile: 'yarn.lock',
+			run: (script, args) => `yarn ${script} ${args || ''}`,
+		}),
+	})[packageManager]()
 
-const getPackageManagerVersion = (packageManager) =>
-    // Copied over from https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L105-L114
-    execSync(`${packageManager} --version`).toString("utf-8").trim()
+const getPackageManagerVersion = packageManager =>
+	// Copied over from https://github.com/nrwl/nx/blob/bd9b33eaef0393d01f747ea9a2ac5d2ca1fb87c6/packages/nx/src/utils/package-manager.ts#L105-L114
+	execSync(`${packageManager} --version`).toString('utf-8').trim()
 
-const getRandomString = (length) => crypto.randomBytes(length).toString("hex")
+const getRandomString = length => crypto.randomBytes(length).toString('hex')
 
 const readFileIfNotTypeScript = (
-    isTypeScript,
-    filePath,
-    parseFunction = (result) => result
+	isTypeScript,
+	filePath,
+	parseFunction = result => result,
 ) =>
-    isTypeScript
-        ? Promise.resolve()
-        : fs.readFile(filePath, "utf-8").then(parseFunction)
+	isTypeScript
+		? Promise.resolve()
+		: fs.readFile(filePath, 'utf-8').then(parseFunction)
 
 const removeUnusedDependencies = (dependencies, unusedDependencies) =>
-    Object.fromEntries(
-        Object.entries(dependencies).filter(
-            ([key]) => !unusedDependencies.includes(key)
-        )
-    )
+	Object.fromEntries(
+		Object.entries(dependencies).filter(
+			([key]) => !unusedDependencies.includes(key),
+		),
+	)
 
-const updatePackageJson = ({APP_NAME, isTypeScript, packageJson}) => {
-    const {
-        devDependencies,
-        prisma: {seed: prismaSeed, ...prisma},
-        scripts: {
-            "format:repo": _repoFormatScript,
-            "lint:repo": _repoLintScript,
-            typecheck,
-            validate,
-            ...scripts
-        },
-    } = packageJson.content
+const updatePackageJson = ({ APP_NAME, isTypeScript, packageJson }) => {
+	const {
+		devDependencies,
+		prisma: { seed: prismaSeed, ...prisma },
+		scripts: {
+			'format:repo': _repoFormatScript,
+			'lint:repo': _repoLintScript,
+			typecheck,
+			validate,
+			...scripts
+		},
+	} = packageJson.content
 
-    packageJson.update({
-        name: APP_NAME,
-        devDependencies: removeUnusedDependencies(
-            devDependencies,
-            // packages that are only used for linting the repo
-            ["eslint-plugin-markdown", "eslint-plugin-prefer-let"].concat(
-                isTypeScript ? [] : ["ts-node"]
-            )
-        ),
-        prisma: isTypeScript
-            ? {...prisma, seed: prismaSeed}
-            : {
-                ...prisma,
-                seed: prismaSeed
-                    .replace("ts-node", "node")
-                    .replace("seed.ts", "seed.js"),
-            },
-        scripts: isTypeScript
-            ? {...scripts, typecheck, validate}
-            : {...scripts, validate: validate.replace(" typecheck", "")},
-    })
+	packageJson.update({
+		name: APP_NAME,
+		devDependencies: removeUnusedDependencies(
+			devDependencies,
+			// packages that are only used for linting the repo
+			['eslint-plugin-markdown', 'eslint-plugin-prefer-let'].concat(
+				isTypeScript ? [] : ['ts-node'],
+			),
+		),
+		prisma: isTypeScript
+			? { ...prisma, seed: prismaSeed }
+			: {
+					...prisma,
+					seed: prismaSeed
+						.replace('ts-node', 'node')
+						.replace('seed.ts', 'seed.js'),
+			  },
+		scripts: isTypeScript
+			? { ...scripts, typecheck, validate }
+			: { ...scripts, validate: validate.replace(' typecheck', '') },
+	})
 }
 
-const main = async ({isTypeScript, packageManager, rootDirectory}) => {
-    const pm = getPackageManagerCommand(packageManager)
-    const FILE_EXTENSION = isTypeScript ? "ts" : "js"
+const main = async ({ isTypeScript, packageManager, rootDirectory }) => {
+	const pm = getPackageManagerCommand(packageManager)
+	const FILE_EXTENSION = isTypeScript ? 'ts' : 'js'
 
-    const README_PATH = path.join(rootDirectory, "README.md")
-    const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example")
-    const ENV_PATH = path.join(rootDirectory, ".env")
-    const DEPLOY_WORKFLOW_PATH = path.join(
-        rootDirectory,
-        ".github",
-        "workflows",
-        "ci.yml"
-    )
-    const DOCKERFILE_PATH = path.join(rootDirectory, "Dockerfile")
-    const CYPRESS_SUPPORT_PATH = path.join(rootDirectory, "cypress", "support")
-    const CYPRESS_COMMANDS_PATH = path.join(
-        CYPRESS_SUPPORT_PATH,
-        `commands.${FILE_EXTENSION}`
-    )
-    const CREATE_USER_COMMAND_PATH = path.join(
-        CYPRESS_SUPPORT_PATH,
-        `create-user.${FILE_EXTENSION}`
-    )
-    const DELETE_USER_COMMAND_PATH = path.join(
-        CYPRESS_SUPPORT_PATH,
-        `delete-user.${FILE_EXTENSION}`
-    )
-    const CREATE_VERIFICATION_COMMAND_PATH = path.join(
-        CYPRESS_SUPPORT_PATH,
-        `create-verification.${FILE_EXTENSION}`
-    )
-    const VITEST_CONFIG_PATH = path.join(
-        rootDirectory,
-        `vitest.config.${FILE_EXTENSION}`
-    )
+	const README_PATH = path.join(rootDirectory, 'README.md')
+	const EXAMPLE_ENV_PATH = path.join(rootDirectory, '.env.example')
+	const ENV_PATH = path.join(rootDirectory, '.env')
+	const DEPLOY_WORKFLOW_PATH = path.join(
+		rootDirectory,
+		'.github',
+		'workflows',
+		'ci.yml',
+	)
+	const DOCKERFILE_PATH = path.join(rootDirectory, 'Dockerfile')
+	const CYPRESS_SUPPORT_PATH = path.join(rootDirectory, 'cypress', 'support')
+	const CYPRESS_COMMANDS_PATH = path.join(
+		CYPRESS_SUPPORT_PATH,
+		`commands.${FILE_EXTENSION}`,
+	)
+	const CREATE_USER_COMMAND_PATH = path.join(
+		CYPRESS_SUPPORT_PATH,
+		`create-user.${FILE_EXTENSION}`,
+	)
+	const DELETE_USER_COMMAND_PATH = path.join(
+		CYPRESS_SUPPORT_PATH,
+		`delete-user.${FILE_EXTENSION}`,
+	)
+	const CREATE_VERIFICATION_COMMAND_PATH = path.join(
+		CYPRESS_SUPPORT_PATH,
+		`create-verification.${FILE_EXTENSION}`,
+	)
+	const VITEST_CONFIG_PATH = path.join(
+		rootDirectory,
+		`vitest.config.${FILE_EXTENSION}`,
+	)
 
-    const REPLACER = "nobu-stack-template"
+	const REPLACER = 'nobu-stack-template'
 
-    const DIR_NAME = path.basename(rootDirectory)
-    const SUFFIX = getRandomString(2)
+	const DIR_NAME = path.basename(rootDirectory)
+	const SUFFIX = getRandomString(2)
 
-    const APP_NAME = (DIR_NAME + "-" + SUFFIX)
-        // get rid of anything that's not allowed in an app name
-        .replace(/[^a-zA-Z0-9-_]/g, "-")
+	const APP_NAME = (DIR_NAME + '-' + SUFFIX)
+		// get rid of anything that's not allowed in an app name
+		.replace(/[^a-zA-Z0-9-_]/g, '-')
 
-    const [
-        readme,
-        env,
-        dockerfile,
-        cypressCommands,
-        createUserCommand,
-        deleteUserCommand,
-        createVerificationCommand,
-        deployWorkflow,
-        vitestConfig,
-        packageJson,
-    ] = await Promise.all([
-        fs.readFile(README_PATH, "utf-8"),
-        fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
-        fs.readFile(DOCKERFILE_PATH, "utf-8"),
-        fs.readFile(CYPRESS_COMMANDS_PATH, "utf-8"),
-        fs.readFile(CREATE_USER_COMMAND_PATH, "utf-8"),
-        fs.readFile(DELETE_USER_COMMAND_PATH, "utf-8"),
-        fs.readFile(CREATE_VERIFICATION_COMMAND_PATH, "utf-8"),
-        readFileIfNotTypeScript(isTypeScript, DEPLOY_WORKFLOW_PATH, (s) =>
-            YAML.parse(s)
-        ),
-        readFileIfNotTypeScript(isTypeScript, VITEST_CONFIG_PATH),
-        PackageJson.load(rootDirectory),
-    ])
+	const [
+		readme,
+		env,
+		dockerfile,
+		cypressCommands,
+		createUserCommand,
+		deleteUserCommand,
+		createVerificationCommand,
+		deployWorkflow,
+		vitestConfig,
+		packageJson,
+	] = await Promise.all([
+		fs.readFile(README_PATH, 'utf-8'),
+		fs.readFile(EXAMPLE_ENV_PATH, 'utf-8'),
+		fs.readFile(DOCKERFILE_PATH, 'utf-8'),
+		fs.readFile(CYPRESS_COMMANDS_PATH, 'utf-8'),
+		fs.readFile(CREATE_USER_COMMAND_PATH, 'utf-8'),
+		fs.readFile(DELETE_USER_COMMAND_PATH, 'utf-8'),
+		fs.readFile(CREATE_VERIFICATION_COMMAND_PATH, 'utf-8'),
+		readFileIfNotTypeScript(isTypeScript, DEPLOY_WORKFLOW_PATH, s =>
+			YAML.parse(s),
+		),
+		readFileIfNotTypeScript(isTypeScript, VITEST_CONFIG_PATH),
+		PackageJson.load(rootDirectory),
+	])
 
-    const newEnv = env.replace(
-        /^SESSION_SECRET=.*$/m,
-        `SESSION_SECRET="${getRandomString(16)}"`
-    ).replace(
-        /^ARGON_SECRET_KEY=.*$/m,
-        `ARGON_SECRET_KEY="${getRandomString(16)}"`
-    ).replace(
-        /^COOKIE_SECRETS=.*$/m,
-        `COOKIE_SECRETS="${getRandomString(32)}"`
-    )
+	const newEnv = env
+		.replace(/^SESSION_SECRET=.*$/m, `SESSION_SECRET="${getRandomString(16)}"`)
+		.replace(
+			/^ARGON_SECRET_KEY=.*$/m,
+			`ARGON_SECRET_KEY="${getRandomString(16)}"`,
+		)
+		.replace(/^COOKIE_SECRETS=.*$/m, `COOKIE_SECRETS="${getRandomString(32)}"`)
 
-
-    const initInstructions = `
+	const initInstructions = `
 - First run this stack's \`remix.init\` script and commit the changes it makes to your project.
 
   \`\`\`sh
@@ -227,77 +223,77 @@ const main = async ({isTypeScript, packageManager, rootDirectory}) => {
   \`\`\`
 `
 
-    const newReadme = readme
-        .replace(new RegExp(escapeRegExp(REPLACER), "g"), APP_NAME)
-        .replace(initInstructions, "")
+	const newReadme = readme
+		.replace(new RegExp(escapeRegExp(REPLACER), 'g'), APP_NAME)
+		.replace(initInstructions, '')
 
-    const newDockerfile = pm.lockfile
-        ? dockerfile.replace(
-            new RegExp(escapeRegExp("ADD package.json"), "g"),
-            `ADD package.json ${pm.lockfile}`
-        )
-        : dockerfile
+	const newDockerfile = pm.lockfile
+		? dockerfile.replace(
+				new RegExp(escapeRegExp('ADD package.json'), 'g'),
+				`ADD package.json ${pm.lockfile}`,
+		  )
+		: dockerfile
 
-    updatePackageJson({APP_NAME, isTypeScript, packageJson})
+	updatePackageJson({ APP_NAME, isTypeScript, packageJson })
 
-    const fileOperationPromises = [
-        fs.writeFile(README_PATH, newReadme),
-        fs.writeFile(ENV_PATH, newEnv),
-        fs.writeFile(DOCKERFILE_PATH, newDockerfile),
-        ...cleanupCypressFiles({
-            fileEntries: [
-                [CYPRESS_COMMANDS_PATH, cypressCommands],
-                [CREATE_USER_COMMAND_PATH, createUserCommand],
-                [DELETE_USER_COMMAND_PATH, deleteUserCommand],
-                [CREATE_VERIFICATION_COMMAND_PATH, createVerificationCommand],
-            ],
-            isTypeScript,
-            packageManager: pm,
-        }),
-        packageJson.save(),
-        fs.copyFile(
-            path.join(rootDirectory, "remix.init", "gitignore"),
-            path.join(rootDirectory, ".gitignore")
-        ),
-        fs.rm(path.join(rootDirectory, ".github", "dependabot.yml")),
-        fs.rm(path.join(rootDirectory, ".eslintrc.repo.js")),
-        fs.rm(path.join(rootDirectory, "LICENSE.md")),
-    ]
+	const fileOperationPromises = [
+		fs.writeFile(README_PATH, newReadme),
+		fs.writeFile(ENV_PATH, newEnv),
+		fs.writeFile(DOCKERFILE_PATH, newDockerfile),
+		...cleanupCypressFiles({
+			fileEntries: [
+				[CYPRESS_COMMANDS_PATH, cypressCommands],
+				[CREATE_USER_COMMAND_PATH, createUserCommand],
+				[DELETE_USER_COMMAND_PATH, deleteUserCommand],
+				[CREATE_VERIFICATION_COMMAND_PATH, createVerificationCommand],
+			],
+			isTypeScript,
+			packageManager: pm,
+		}),
+		packageJson.save(),
+		fs.copyFile(
+			path.join(rootDirectory, 'remix.init', 'gitignore'),
+			path.join(rootDirectory, '.gitignore'),
+		),
+		fs.rm(path.join(rootDirectory, '.github', 'dependabot.yml')),
+		fs.rm(path.join(rootDirectory, '.eslintrc.repo.js')),
+		fs.rm(path.join(rootDirectory, 'LICENSE.md')),
+	]
 
-    if (!isTypeScript) {
-        fileOperationPromises.push(
-            ...cleanupDeployWorkflow(deployWorkflow, DEPLOY_WORKFLOW_PATH)
-        )
+	if (!isTypeScript) {
+		fileOperationPromises.push(
+			...cleanupDeployWorkflow(deployWorkflow, DEPLOY_WORKFLOW_PATH),
+		)
 
-        fileOperationPromises.push(
-            ...cleanupVitestConfig(vitestConfig, VITEST_CONFIG_PATH)
-        )
-    }
+		fileOperationPromises.push(
+			...cleanupVitestConfig(vitestConfig, VITEST_CONFIG_PATH),
+		)
+	}
 
-    await Promise.all(fileOperationPromises)
+	await Promise.all(fileOperationPromises)
 
-    execSync(pm.run("format", "--loglevel warn"), {
-        cwd: rootDirectory,
-        stdio: "inherit",
-    })
+	execSync(pm.run('format', '--loglevel warn'), {
+		cwd: rootDirectory,
+		stdio: 'inherit',
+	})
 
-    console.log(
-        `
+	console.log(
+		`
 Setup is almost complete. Follow these steps to finish initialization:
 
 - Start the database:
-  ${pm.run("docker")}
+  ${pm.run('docker')}
 
 - Run setup (this updates the database):
-  ${pm.run("setup")}
+  ${pm.run('setup')}
 
 - Run the first build (this generates the server you will run):
-  ${pm.run("build")}
+  ${pm.run('build')}
 
 - You're now ready to rock and roll 🤘
-  ${pm.run("dev")}
-    `.trim()
-    )
+  ${pm.run('dev')}
+    `.trim(),
+	)
 }
 
 module.exports = main
