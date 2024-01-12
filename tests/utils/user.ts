a99510893bb284843083b8da02e8d7902cf171e3
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 13:10:08 +0000

    feat: ensure generate email address ends with @example.com

--- tests/utils/user.ts
+++ tests/utils/user.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 
 export function generateUserCredentials(email?: string, password?: string) {
 	return {
-		email: email ?? faker.internet.email(),
+		email: email ?? faker.internet.email({ provider: 'example.com' }),
 		password:
 			password ?? faker.internet.password({ length: 8, memorable: false }),
 	}
