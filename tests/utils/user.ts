397aaedd9160099a5979c82ccf1a3678d934ba4a
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 13:10:52 +0000

    Add validation for example.com email addresses

--- tests/utils/user.ts
+++ tests/utils/user.ts
@@ -1,6 +1,10 @@
 import { faker } from '@faker-js/faker'
 
 export function generateUserCredentials(email?: string, password?: string) {
+	if (!email.endsWith('@example.com')) {
+		throw new Error('Email must be an example.com email')
+	}
+
 	return {
 		email: email ?? faker.internet.email({ provider: 'example.com' }),
 		password:
