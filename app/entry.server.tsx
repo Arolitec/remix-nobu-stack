1703fe7b03682eb1d540c5b31009a8cc77e200e1
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 17:28:22 +0000

    refactor: Update isbot import in entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,7 +11,7 @@ import {
 	type EntryContext,
 } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
-import isbot from 'isbot'
+import { isbot } from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
 
 const ABORT_DELAY = 5_000
@@ -22,7 +22,7 @@ export default function handleRequest(
 	responseHeaders: Headers,
 	remixContext: EntryContext,
 ) {
-	return isbot(request.headers.get('user-agent'))
+	return isbot(request.headers.get('user-agent') || '')
 		? handleBotRequest(
 				request,
 				responseStatusCode,
