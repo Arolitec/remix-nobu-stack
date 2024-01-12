22cf1966c6f0f4e87b5b0fd381a14d4dac68129c
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 22:10:38 +0000

    refactor: Refactor welcome email component to fix issue
    set props as readonly

--- app/routes/_auth+/join+/welcome.email.server.tsx
+++ app/routes/_auth+/join+/welcome.email.server.tsx
@@ -1,14 +1,15 @@
 import {
+	Body,
 	Button,
 	Container,
+	Font,
+	Head,
 	Hr,
 	Html,
 	Preview,
 	Section,
-	Text,
 	Tailwind,
-	Font,
-	Head,
+	Text,
 } from '@react-email/components'
 import tailwindConfig from '../../../../tailwind.config'
 
@@ -16,45 +17,47 @@ interface WelcomeEmailProps {
 	username: string
 }
 
-export default function WelcomeEmail(props: WelcomeEmailProps) {
+export default function WelcomeEmail(props: Readonly<WelcomeEmailProps>) {
 	const { username } = props
 
 	return (
-		<Tailwind config={tailwindConfig}>
-			<Html lang="en" data-theme="light">
+		<Html lang="en" data-theme="light">
+			<Preview>Welcome to Nobu Stack</Preview>
+			<Tailwind config={tailwindConfig}>
 				<Head>
 					<Font fontFamily="Nunito Sans" fallbackFontFamily="Verdana" />
 				</Head>
-				<Preview>Welcome to Nobu Stack.</Preview>
-				<Container>
-					<Section className="flex w-full items-center justify-center">
-						<Text className="text-4xl font-bold uppercase">Nobu Stack</Text>
-					</Section>
-					<Section>
-						<Text className="text-base">Hi {username},</Text>
+				<Body>
+					<Container>
+						<Section className="flex w-full items-center justify-center">
+							<Text className="text-4xl font-bold uppercase">Nobu Stack</Text>
+						</Section>
+						<Section>
+							<Text className="text-base">Hi {username},</Text>
+							<Text className="text-base">
+								Welcome to Nobu Stack. We hope you will like it!
+							</Text>
+						</Section>
+						<Section className="my-4 flex items-center justify-center">
+							<Button
+								href="http://localhost:3000/login"
+								className="btn-primary btn "
+							>
+								Get started
+							</Button>
+						</Section>
 						<Text className="text-base">
-							Welcome to Nobu Stack. We hope you will like it!
+							Best,
+							<br />
+							Arolitec Team
 						</Text>
-					</Section>
-					<Section className="my-4 flex items-center justify-center">
-						<Button
-							href="http://localhost:3000/login"
-							className="btn-primary btn "
-						>
-							Get started
-						</Button>
-					</Section>
-					<Text className="text-base">
-						Best,
-						<br />
-						Arolitec Team
-					</Text>
-					<Hr />
-					<Text className="text-base">
-						All rights reserved. {new Date().getFullYear()}
-					</Text>
-				</Container>
-			</Html>
-		</Tailwind>
+						<Hr />
+						<Text className="text-base">
+							All rights reserved. {new Date().getFullYear()}
+						</Text>
+					</Container>
+				</Body>
+			</Tailwind>
+		</Html>
 	)
 }
