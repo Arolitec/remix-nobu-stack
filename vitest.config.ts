62e772b101a7bee44fa6970ba8d191258c64ebdf
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 18:57:06 +0000

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,8 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// @ts-ignore
-import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
