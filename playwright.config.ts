8ac2f0d969b08d6344d63199aba2d7986749e781
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-12 13:59:48 +0000

    chore: Update playwright.config.ts with dynamic port configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test'
  */
 // require('dotenv').config();
 
+const PORT = process.env.PORT || '3000'
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -14,28 +16,16 @@ export default defineConfig({
 	expect: {
 		timeout: 10000,
 	},
-	/* Run tests in files in parallel */
 	fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:3000',
-
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		baseURL: `http://localhost:${PORT}`,
 		trace: 'on-first-retry',
 	},
-
-	/* Configure projects for major browsers */
 	projects: [
-		// Setup
 		{
 			name: 'test env setup',
 			testMatch: /test-env\.ts/,
@@ -46,11 +36,15 @@ export default defineConfig({
 			dependencies: ['test env setup'],
 		},
 	],
-
-	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'yarn dev',
-		url: 'http://localhost:3000',
+		command: `yarn start-server-and-test ${
+			process.env.CI ? 'start:mocks' : 'dev'
+		} http://127.0.0.1:${PORT}`,
 		reuseExistingServer: !process.env.CI,
+		port: Number(PORT),
+		stderr: 'pipe',
+		env: {
+			PORT,
+		},
 	},
 })
