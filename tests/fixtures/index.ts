588aa20aeecaf4248901deb7ac65699834ee8c9a
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-13 15:04:00 +0000

    Remove unused imports and functions

--- tests/fixtures/index.ts
+++ tests/fixtures/index.ts
@@ -1,11 +1,6 @@
-import { faker } from '@faker-js/faker'
 import type { Cookie } from '@playwright/test'
 import { test as base } from '@playwright/test'
-import type { User } from '@prisma/client'
 import { installGlobals } from '@remix-run/node'
-import execa from 'execa'
-import { prisma } from '~/utils/db.server'
-import { generateTOTP } from '~/utils/otp.server'
 import { createSessionCookie } from '../utils/auth'
 import { PasswordForgottenPage } from './password-forgotten-page'
 import { PasswordResetPage } from './password-rest-page'
@@ -15,10 +10,6 @@ import { SignUpPage } from './signup-page'
 installGlobals()
 
 type Fixtures = {
-	setupDb: () => Promise<void>
-	createUser: (email?: string, password?: string) => Promise<User>
-	createManyUsers: (count: number) => Promise<User[]>
-	createVerification: (email?: string) => Promise<string>
 	login: (email?: string, password?: string) => Promise<void>
 	getSessionCookie: () => Promise<Cookie | undefined>
 	signupPage: SignUpPage
@@ -28,31 +19,6 @@ type Fixtures = {
 }
 
 const test = base.extend<Fixtures>({
-	// eslint-disable-next-line no-empty-pattern
-	setupDb: async ({}, use) => {
-		await use(async () => {
-			await execa('yarn', ['prisma', 'migrate', 'reset', '-f'], {
-				stdio: 'inherit',
-			})
-		})
-	},
-	// eslint-disable-next-line no-empty-pattern
-	createUser: async ({}, use) => {
-		await use(
-			(
-				email = faker.internet.email(),
-				password = faker.internet.password(),
-			) => {
-				return prisma.user.createUser(email, password)
-			},
-		)
-	},
-	createManyUsers: async ({ createUser }, use) => {
-		await use(async (count = 1) => {
-			const promises$ = Array.from({ length: count }).map(() => createUser())
-			return Promise.all(promises$)
-		})
-	},
 	login: async ({ context }, use) => {
 		await use(async (email?: string, password?: string) => {
 			const cookie = await createSessionCookie(email, password)
@@ -68,24 +34,6 @@ const test = base.extend<Fixtures>({
 					sameSite: 'Lax',
 				},
 			])
-		})
-	},
-	createVerification: async ({}, use) => {
-		await use(async (email = faker.internet.email()) => {
-			const otpData = generateTOTP()
-
-			await prisma.verification.create({
-				data: {
-					algorithm: otpData.algorithm,
-					digits: 6,
-					email,
-					period: otpData.step,
-					expiresAt: otpData.expiresAt,
-					secret: otpData.secret,
-				},
-			})
-
-			return otpData.otp
 		})
 	},
 	getSessionCookie: async ({ context }, use) => {
