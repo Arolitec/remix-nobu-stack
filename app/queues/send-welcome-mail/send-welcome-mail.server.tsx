02b7883c48ddeb90cfdbc1a8840f45abbe5e1d44
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-17 09:15:44 +0000

    refactor: fix typo on queue route path

--- app/queues/send-welcome-mail/send-welcome-mail.server.tsx
+++ app/queues/send-welcome-mail/send-welcome-mail.server.tsx
@@ -4,12 +4,14 @@ import { Queue } from 'quirrel/remix'
 import { sendMail } from '~/utils/mailer.server'
 import WelcomeEmail from './welcome.email.server'
 
-export default Queue(
-	'queues/welcome-mail/send-welcome-mail',
-	async (user: User) => {
+export default Queue<User>(
+	'queues/send-welcome-email',
+	async user => {
 		const html = render(<WelcomeEmail username={user.email} />)
+		const subject = 'Welcome to Nobu Stack'
 
-		await sendMail(user.email, 'Welcome to my app!', html)
+		await sendMail(user.email, subject, html)
+		console.log(`Sent welcome email to ${user.email}`)
 	},
 	{ retry: ['1min', '5min', '10min'] },
 )
--- app/routes/queues/send-welcome-mail.server.ts
+++
@@ -1,3 +0,0 @@
-import welcomeMailQueue from '~/queues/welcome-mail/send-welcome-mail.server'
-
-export const action = welcomeMailQueue
