0e53cf8cf4e736f55112dc047b29b0d1ad254e26
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-27 22:44:01 +0000

    update vitest config to properly handle quirrel module

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,20 +6,27 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		setupFiles: ['./test/setup-test-env.ts'],
-		include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		watchExclude: [
-			'.*\\/node_modules\\/.*',
-			'.*\\/build\\/.*',
-			'.*\\/postgres-data\\/.*',
-			'.*\\/playwright-report\\/.*',
-		],
-		poolMatchGlobs: [
-			['**/(loader|action).test.{js,mjs,cjs,ts,mts,cts}', 'forks'],
-		],
-	},
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./test/setup-test-env.ts'],
+    include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    watchExclude: [
+      '.*\\/node_modules\\/.*',
+      '.*\\/build\\/.*',
+      '.*\\/postgres-data\\/.*',
+      '.*\\/playwright-report\\/.*',
+    ],
+    poolMatchGlobs: [
+      ['**/(loader|action).test.{js,mjs,cjs,ts,mts,cts}', 'forks'],
+    ],
+    server: {
+      deps: {
+        inline: [
+          "quirrel"
+        ]
+      }
+    }
+  },
 })
