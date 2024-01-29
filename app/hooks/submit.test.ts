34a0e8a347dab2f1622dfff992a3b506c5c1cc08
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-29 14:38:43 +0000

    Refactor useSubmitting hook to handle page submission state correctly

--- app/hooks/submit.test.ts
+++ app/hooks/submit.test.ts
@@ -1,35 +1,35 @@
-import useSubmitting from "./submit"
-import { renderHook } from "@testing-library/react"
-import { useNavigation } from "@remix-run/react"
-import type { Mock } from "vitest"
+import { useNavigation } from '@remix-run/react'
+import { renderHook } from '@testing-library/react'
+import type { Mock } from 'vitest'
+import useSubmitting from './submit'
 
-vi.mock("@remix-run/react", async (importModule) => ({
-  ...await importModule(),
-  useNavigation: vi.fn(),
+vi.mock('@remix-run/react', async importModule => ({
+	...(await importModule()),
+	useNavigation: vi.fn(),
 }))
 
-describe("useSubmitting", () => {
-  it('should return true if page is submitting', async () => {
-    (useNavigation as Mock).mockReturnValue({ state: 'submitting' })
+describe('useSubmitting', () => {
+	it('should return true if page is submitting', async () => {
+		;(useNavigation as Mock).mockReturnValue({ state: 'submitting' })
 
-    const { result } = renderHook(() => useSubmitting())
+		const { result } = renderHook(() => useSubmitting())
 
-    expect(result.current).toBe(true)
-  })
+		expect(result.current).toBe(true)
+	})
 
-  it('should return true if page is loading', async () => {
-    (useNavigation as Mock).mockReturnValue({ state: 'loading' })
+	it('should return false if page is loading', async () => {
+		;(useNavigation as Mock).mockReturnValue({ state: 'loading' })
 
-    const { result } = renderHook(() => useSubmitting())
+		const { result } = renderHook(() => useSubmitting())
 
-    expect(result.current).toBe(true)
-  })
+		expect(result.current).toBe(false)
+	})
 
-  it('should return false if page is not submitting', async () => {
-    (useNavigation as Mock).mockReturnValue({ state: 'idle' })
+	it('should return false if page is not submitting', async () => {
+		;(useNavigation as Mock).mockReturnValue({ state: 'idle' })
 
-    const { result } = renderHook(() => useSubmitting())
+		const { result } = renderHook(() => useSubmitting())
 
-    expect(result.current).toBe(false)
-  })
+		expect(result.current).toBe(false)
+	})
 })
--- app/hooks/submit.ts
+++ app/hooks/submit.ts
@@ -1,10 +1,10 @@
-import { useNavigation } from "@remix-run/react"
-import { useMemo } from "react"
+import { useNavigation } from '@remix-run/react'
+import { useMemo } from 'react'
 
 export default function useSubmitting() {
-  const { state } = useNavigation()
+	const { state } = useNavigation()
 
-  const isSubmitting = useMemo(() => ['submitting', 'loading'].includes(state), [state])
+	const isSubmitting = useMemo(() => ['submitting'].includes(state), [state])
 
-  return isSubmitting
+	return isSubmitting
 }
