28082f4ae14870da2e90ca4f12a5b0900cc977bd
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-30 10:19:50 +0000

    Add testing-library/no-await-sync-events rule to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,14 @@ module.exports = {
 				'testing-library/prefer-screen-queries': 'off',
 			},
 		},
+		{
+			'testing-library/no-await-sync-events': [
+				'error',
+				{
+					eventModules: ['fire-event'],
+				},
+			],
+		},
 	],
 	// We're using vitest which has a very similar API to jest
 	// (so the linting plugins work nicely), but we have to
