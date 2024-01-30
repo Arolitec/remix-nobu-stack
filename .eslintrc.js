27595ad5ed7dd12ff0abe26121eb3da0f6e17bca
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-30 08:52:43 +0000

    chore: Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,20 @@ module.exports = {
 		'plugin:playwright/recommended',
 		'prettier',
 	],
+	overrides: [
+		{
+			files: ['app/**'],
+			rules: {
+				'playwright/missing-playwright-await': 'off',
+			},
+		},
+		{
+			files: ['tests/e2e/**'],
+			rules: {
+				'testing-library/prefer-screen-queries': 'off',
+			},
+		},
+	],
 	// We're using vitest which has a very similar API to jest
 	// (so the linting plugins work nicely), but we have to
 	// set the jest version explicitly.
