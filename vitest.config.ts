b8d517de39f931aaaee0a09c78f502d3052594f4
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-30 12:16:22 +0000

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,27 +6,31 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['./test/setup-test-env.ts'],
-    include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    watchExclude: [
-      '.*\\/node_modules\\/.*',
-      '.*\\/build\\/.*',
-      '.*\\/postgres-data\\/.*',
-      '.*\\/playwright-report\\/.*',
-    ],
-    poolMatchGlobs: [
-      ['**/(loader|action).test.{js,mjs,cjs,ts,mts,cts}', 'forks'],
-    ],
-    server: {
-      deps: {
-        inline: [
-          "quirrel"
-        ]
-      }
-    }
-  },
+	plugins: [react(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'happy-dom',
+		setupFiles: ['./test/setup-test-env.ts'],
+		include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		exclude: ['.tests/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		watchExclude: [
+			'.*\\/node_modules\\/.*',
+			'.*\\/build\\/.*',
+			'.*\\/postgres-data\\/.*',
+			'.*\\/playwright-report\\/.*',
+		],
+		poolMatchGlobs: [
+			['**/(loader|action).test.{js,mjs,cjs,ts,mts,cts}', 'forks'],
+		],
+		server: {
+			deps: {
+				inline: ['quirrel'],
+			},
+		},
+		coverage: {
+			reporter: [process.env.CI ? 'json' : 'html'],
+			enabled: true,
+			clean: true,
+		},
+	},
 })
