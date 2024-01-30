80bd59bca3af60e8f3822da69b0a27131e4e4123
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-30 15:08:14 +0000

    refactor: Refactor join action in auth route

--- app/routes/_auth+/join+/action.tsx
+++ app/routes/_auth+/join+/action.tsx
@@ -8,49 +8,49 @@ import { prisma } from '~/utils/db.server'
 import { safeRedirect } from '~/utils/redirect'
 
 export const clientSchema = z.object({
-  email: z.coerce.string().email('You must enter a valid mail address'),
-  password: z.coerce.string().min(8, 'Password must be at least 8 characters'),
+	email: z.coerce.string().email('You must enter a valid mail address'),
+	password: z.coerce.string().min(8, 'Password must be at least 8 characters'),
 })
 
 const schema = clientSchema.superRefine(async (data, ctx) => {
-  const { email } = data
+	const { email } = data
 
-  const existingUser = await prisma.user.findUnique({
-    where: { email: email },
-  })
+	const existingUser = await prisma.user.findUnique({
+		where: { email: email },
+	})
 
-  if (existingUser) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: 'A user already exists with this email',
-      path: ['email'],
-    })
-  }
+	if (existingUser) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.custom,
+			message: 'A user already exists with this email',
+			path: ['email'],
+		})
+	}
 })
 
 const action = async ({ request }: ActionFunctionArgs) => {
-  const formData = await request.formData()
-  const submission = await parse(formData, {
-    schema,
-    async: true,
-  })
-  const redirectTo = safeRedirect(formData.get('redirectTo'), '/')
+	const formData = await request.formData()
+	const submission = await parse(formData, {
+		schema,
+		async: true,
+	})
+	const redirectTo = safeRedirect(formData.get('redirectTo'), '/')
 
-  if (!submission.value || submission.intent !== 'submit') {
-    return json(submission, { status: 400 })
-  }
+	if (!submission.value || submission.intent !== 'submit') {
+		return json(submission, { status: 400 })
+	}
 
-  const user = await prisma.user.createUser(
-    submission.value.email,
-    submission.value.password,
-  )
+	const user = await prisma.user.createUser(
+		submission.value.email,
+		submission.value.password,
+	)
 
-  sendWelcomeEmailQueue.enqueue(user, { delay: '1s' })
+	sendWelcomeEmailQueue.enqueue(user, { delay: '1s' })
 
-  return authenticator.authenticate(FormStrategy.name, request, {
-    successRedirect: redirectTo,
-    context: { formData },
-  })
+	return authenticator.authenticate(FormStrategy.name, request, {
+		successRedirect: redirectTo,
+		context: { formData },
+	})
 }
 
 export default action
