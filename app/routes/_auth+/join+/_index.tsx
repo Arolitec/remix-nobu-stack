9b28cac0cca2cd1e18d6485a258a74adebd6ad18
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-30 13:44:14 +0000

    refactor: Refactor Join page component

--- app/routes/_auth+/join+/_index.tsx
+++ app/routes/_auth+/join+/_index.tsx
@@ -1,8 +1,9 @@
 import type { MetaFunction } from '@remix-run/node'
+import { Link, useSearchParams } from '@remix-run/react'
 import { GeneralErrorBoundary } from '~/components/error-boundary'
 import actionFn from './action'
+import { JoinForm } from './components/join-form'
 import loaderFn from './loader'
-import JoinForm from './components/JoinForm'
 
 export const loader = loaderFn
 
@@ -11,15 +12,33 @@ export const action = actionFn
 export const meta: MetaFunction = () => [{ title: 'Sign Up' }]
 
 export default function Join() {
-  return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        <JoinForm />
-      </div>
-    </div>
-  )
+	const [searchParams] = useSearchParams()
+
+	return (
+		<div className="flex min-h-full flex-col justify-center">
+			<div className="mx-auto w-full max-w-md px-8">
+				<JoinForm />
+
+				<div className="flex items-center justify-center pt-8">
+					<p className="text-center text-sm text-gray-500">
+						Already have an account?{' '}
+						<Link
+							prefetch="intent"
+							className="text-accent underline"
+							to={{
+								pathname: '/login',
+								search: searchParams.toString(),
+							}}
+						>
+							Log in
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export function ErrorBoundary() {
-  return <GeneralErrorBoundary />
+	return <GeneralErrorBoundary />
 }
