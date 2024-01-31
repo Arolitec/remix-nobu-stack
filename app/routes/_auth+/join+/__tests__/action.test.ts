e9d182bbbdf24871ddecff8336299abc9777b923
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-31 15:36:06 +0000

    test: Refactor join action tests

--- app/routes/_auth+/join+/__tests__/action.test.ts
+++ app/routes/_auth+/join+/__tests__/action.test.ts
@@ -1,110 +1,138 @@
-import type { Mock } from 'vitest'
-import action from '../action'
+import { User } from '@prisma/client'
 import queue from '~/queues/send-welcome-mail/send-welcome-mail.server'
 import { prisma } from '~/utils/db.server'
+import { buildFormData } from '~/utils/form-data'
+import action from '../action'
 
 vi.mock('~/queues/send-welcome-mail/send-welcome-mail.server', () => {
-  return {
-    default: {
-      enqueue: vi.fn()
-    },
-  }
+	return {
+		default: {
+			enqueue: vi.fn(),
+		},
+	}
 })
-
 
 vi.mock('~/utils/db.server', async () => {
-  return {
-    prisma: {
-      user: {
-        findUnique: vi.fn(),
-        createUser: vi.fn()
-      }
-    }
-  }
+	return {
+		prisma: {
+			user: {
+				findUnique: vi.fn(),
+				createUser: vi.fn(),
+			},
+		},
+	}
 })
 
-describe("[join] action", () => {
-  const body = new FormData()
-  body.append('email', 'test@example.com')
-  body.append('password', 'password')
-  body.append('intent', 'submit')
+const DEFAULT_FORM_DATA = {
+	email: 'test@example.com',
+	password: 'password',
+	redirectTo: '/',
+}
 
-  const mockUser = { id: '123' }
+function _buildFormData(data?: Record<string, string>) {
+	return buildFormData({ ...DEFAULT_FORM_DATA, ...data })
+}
 
-  beforeEach(() => {
-    (queue.enqueue as Mock).mockResolvedValue(undefined)
-  })
+describe('[join] action', () => {
+	const mockedEnqueue = vi.mocked(queue.enqueue)
+	const mockedCreateUser = vi.mocked(prisma.user.createUser)
+	const mockedFindUnique = vi.mocked(prisma.user.findUnique)
 
-  afterEach(() => {
-    vi.clearAllMocks()
-  })
+	const mockUser = { id: '123' }
 
-  it.concurrent('should redirect to / if data are valid', async ({ expect }) => {
-    (prisma.user.createUser as Mock).mockResolvedValue(mockUser);
-    (prisma.user.findUnique as Mock).mockResolvedValue(null)
+	beforeEach(() => {
+		mockedEnqueue.mockResolvedValue({} as never)
+	})
 
-    const request = new Request('http://test/join', {
-      method: 'POST',
-      body
-    })
+	afterEach(() => {
+		vi.clearAllMocks()
+	})
 
-    await expect(() => action({
-      request,
-      context: {},
-      params: {}
-    })).rejects.toThrow(Response)
-  })
+	it.concurrent(
+		'should redirect to / if data are valid',
+		async ({ expect }) => {
+			mockedCreateUser.mockResolvedValue(mockUser as User)
+			mockedFindUnique.mockResolvedValue(null)
 
-  it('should queue mail after creating user', async () => {
-    (prisma.user.createUser as Mock).mockResolvedValue(mockUser);
-    (prisma.user.findUnique as Mock).mockResolvedValue(null)
+			const body = _buildFormData()
 
-    const request = new Request('http://test/join', {
-      method: 'POST',
-      body
-    })
+			const request = new Request('http://test/join', {
+				method: 'POST',
+				body,
+			})
 
-    await expect(() => action({
-      request,
-      context: {},
-      params: {}
-    })).rejects.toThrow(Response)
+			await expect(() =>
+				action({
+					request,
+					context: {},
+					params: {},
+				}),
+			).rejects.toThrow(Response)
+		},
+	)
 
-    expect(queue.enqueue).toHaveBeenCalledOnce()
-    expect(queue.enqueue).toHaveBeenCalledWith(mockUser, { delay: '1s' })
-  })
+	it('should queue mail after creating user', async () => {
+		mockedCreateUser.mockResolvedValue(mockUser as User)
+		mockedFindUnique.mockResolvedValue(null)
 
-  it.concurrent('should return a response with 400 status if email is already taken', async () => {
-    (prisma.user.findUnique as Mock).mockResolvedValue(mockUser)
+		const body = _buildFormData()
 
-    const request = new Request('http://test/join', {
-      method: 'POST',
-      body
-    })
+		const request = new Request('http://test/join', {
+			method: 'POST',
+			body,
+		})
 
-    const response = await action({
-      request,
-      context: {},
-      params: {}
-    })
+		await expect(() =>
+			action({
+				request,
+				context: {},
+				params: {},
+			}),
+		).rejects.toThrow(Response)
 
-    expect(response).toBeInstanceOf(Response)
-    expect(response.status).toBe(400)
-  })
+		expect(queue.enqueue).toHaveBeenCalledOnce()
+		expect(queue.enqueue).toHaveBeenCalledWith(mockUser, { delay: '1s' })
+	})
 
-  it.concurrent('should return a response with status 400 if data are invalid', async () => {
-    const request = new Request('http://test/join', {
-      method: 'POST',
-      body: new FormData()
-    })
+	it.concurrent(
+		'should return a response with 400 status if email is already taken',
+		async () => {
+			mockedFindUnique.mockResolvedValue(mockUser as User)
 
-    const response = await action({
-      request,
-      context: {},
-      params: {}
-    })
+			const body = _buildFormData()
 
-    expect(response).toBeInstanceOf(Response)
-    expect(response.status).toBe(400)
-  })
+			const request = new Request('http://test/join', {
+				method: 'POST',
+				body,
+			})
+
+			const response = await action({
+				request,
+				context: {},
+				params: {},
+			})
+
+			expect(response).toBeInstanceOf(Response)
+			expect(response.status).toBe(400)
+		},
+	)
+
+	it.concurrent(
+		'should return a response with status 400 if data are invalid',
+		async () => {
+			const request = new Request('http://test/join', {
+				method: 'POST',
+				body: new FormData(),
+			})
+
+			const response = await action({
+				request,
+				context: {},
+				params: {},
+			})
+
+			expect(response).toBeInstanceOf(Response)
+			expect(response.status).toBe(400)
+		},
+	)
 })
