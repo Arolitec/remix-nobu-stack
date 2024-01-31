4db00dd455c763937c98f3e178a4136d8985119b
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-31 14:52:18 +0000

    test: Refactor login action tests

--- app/routes/_auth+/login+/__tests__/action.test.ts
+++ app/routes/_auth+/login+/__tests__/action.test.ts
@@ -33,13 +33,14 @@ describe('[login] action', () => {
 		expect(response.status).toBe(400)
 	})
 
-	it('should redirect if login succeeded', async () => {
+	it('should redirect to redirectTo url if login succeeded', async () => {
 		;(authenticator.authenticate as Mock).mockResolvedValue({ id: 1 })
 
+		const redirectTo = '/'
 		const body = new FormData()
 		body.append('email', 'test@example.com')
-		body.append('password', 'fakepassword')
-		body.append('redirectTo', '/')
+		body.append('password', 'password')
+		body.append('redirectTo', redirectTo)
 
 		const request = new Request('http://test/login', {
 			method: 'POST',
@@ -49,6 +50,25 @@ describe('[login] action', () => {
 		const response = await action({ request, context: {}, params: {} })
 
 		expect(response.status).toBe(302)
+		expect(response.headers.get('Location')).toBe(redirectTo)
+	})
+
+	it('should set session cookie if login succeeded', async () => {
+		;(authenticator.authenticate as Mock).mockResolvedValue({ id: 1 })
+
+		const body = new FormData()
+		body.append('email', 'test@example.com')
+		body.append('password', 'password')
+		body.append('redirectTo', '/')
+
+		const request = new Request('http://test/login', {
+			method: 'POST',
+			body,
+		})
+
+		const response = await action({ request, context: {}, params: {} })
+
+		expect(response.headers.get('Set-Cookie')).toContain(`_session=`)
 	})
 
 	it('should set max-age cookie if remember is checked on redirect response', async () => {
