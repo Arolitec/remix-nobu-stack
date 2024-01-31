43cfe6eff8e106cc078f5d65ddb7bddd4d123b3d
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-31 16:49:27 +0000

    refactor: Refactor login form component

--- app/routes/_auth+/login+/components/login-form.tsx
+++ app/routes/_auth+/login+/components/login-form.tsx
@@ -1,6 +1,9 @@
 import { conform, useForm } from '@conform-to/react'
 import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import { Form, useActionData } from '@remix-run/react'
+import TextInput from '~/components/form/text-input'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
 import { useRedirectTo } from '../../join+/hooks/redirect'
 import { schema, type Action } from '../action'
 import { PasswordForgottenLink } from './password-forgotten-link'
@@ -29,48 +32,24 @@ export function LoginForm() {
 					<span>{form.error}</span>
 				</div>
 			)}
-			<div className="form-control">
-				<label htmlFor={email.id} className="label">
-					<span className="label-text">Email address</span>
-				</label>
-				<div className="mt-1">
-					<input
-						{...conform.input(email, {
-							type: 'email',
-							ariaAttributes: true,
-						})}
-						required
-						autoFocus={true}
-						autoComplete="email"
-						className={`input-bordered input w-full ${
-							email.error ? 'input-error' : ''
-						}`}
-					/>
-					{!!email.error && (
-						<div className="label-text pt-1 text-error" id={email.errorId}>
-							{email.error}
-						</div>
-					)}
-				</div>
-			</div>
+			<TextInput
+				field={email}
+				label="Email address"
+				InputProps={{ autoComplete: 'email' }}
+			/>
 
-			<div className="form-control">
-				<label htmlFor={password.id} className="label">
-					<span className="label-text">Password</span>
-				</label>
+			<div className="mb-2">
+				<Label htmlFor={password.id}>Password</Label>
 				<div className="mt-1">
-					<input
+					<Input
 						{...conform.input(password, {
 							type: 'password',
 							ariaAttributes: true,
 						})}
 						autoComplete="current-password"
-						className={`input-bordered input w-full ${
-							password.error ? 'input-error' : ''
-						}`}
 					/>
 					{password.error ? (
-						<div className="label-text pt-1 text-error" id={password.errorId}>
+						<div className="label-text pt-1 text-red-500" id={password.errorId}>
 							{password.error}
 						</div>
 					) : null}
