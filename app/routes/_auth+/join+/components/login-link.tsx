4e5942a76e990f73d5ccbe9d95046853c155db72
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-01 10:17:08 +0000

    refactor: Add LoginLink component and its test file

---
+++ app/routes/_auth+/join+/components/login-link.tsx
@@ -0,0 +1,25 @@
+import { Button } from '#/button'
+import { Link, useSearchParams } from '@remix-run/react'
+
+export function LoginLink() {
+	const [searchParams] = useSearchParams()
+
+	return (
+		<div className="flex items-center justify-center pt-8">
+			<p className="text-center text-sm text-zinc-500">
+				Already have an account?{' '}
+				<Button variant="link" className="p-0 h-auto">
+					<Link
+						prefetch="intent"
+						to={{
+							pathname: '/login',
+							search: searchParams.toString(),
+						}}
+					>
+						Log in
+					</Link>
+				</Button>
+			</p>
+		</div>
+	)
+}
