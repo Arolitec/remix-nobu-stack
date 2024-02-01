5e81f32677d5c2cbb274a7eb7b70d687f86b95f9
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-01 10:17:51 +0000

    Remove redundant code and add LoginLink component

--- app/routes/_auth+/join+/_index.tsx
+++ app/routes/_auth+/join+/_index.tsx
@@ -1,8 +1,9 @@
 import type { MetaFunction } from '@remix-run/node'
-import { Link, useSearchParams } from '@remix-run/react'
+import { useSearchParams } from '@remix-run/react'
 import { GeneralErrorBoundary } from '~/components/error-boundary'
 import actionFn from './action'
 import { JoinForm } from './components/join-form'
+import { LoginLink } from './components/login-link'
 import loaderFn from './loader'
 
 export const loader = loaderFn
@@ -19,21 +20,7 @@ export default function Join() {
 			<div className="mx-auto w-full max-w-md px-8">
 				<JoinForm />
 
-				<div className="flex items-center justify-center pt-8">
-					<p className="text-center text-sm text-gray-500">
-						Already have an account?{' '}
-						<Link
-							prefetch="intent"
-							className="text-accent underline"
-							to={{
-								pathname: '/login',
-								search: searchParams.toString(),
-							}}
-						>
-							Log in
-						</Link>
-					</p>
-				</div>
+				<LoginLink />
 			</div>
 		</div>
 	)
