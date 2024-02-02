10f05b684192be68b410b8b5c6c846f66bb14017
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-02 15:57:05 +0000

    Add BackToLoginLink component to PasswordForgottenPage

--- app/routes/_auth+/password-forgotten+/_index.tsx
+++ app/routes/_auth+/password-forgotten+/_index.tsx
@@ -1,6 +1,7 @@
 import { type MetaFunction } from '@remix-run/node'
 import { GeneralErrorBoundary } from '~/components/error-boundary'
 import actionFn from './action'
+import { BackToLoginLink } from './components/back-to-login-link'
 import { PasswordForgottenForm } from './components/password-forgotten-form'
 import loaderFn from './loader'
 
@@ -14,7 +15,13 @@ export default function PasswordForgottenPage() {
 	return (
 		<div className="flex min-h-full flex-col justify-center">
 			<div className="mx-auto w-full max-w-md px-8">
+				<h1 className="text-xl inline-block mb-16 before:content-[''] capitalize before:absolute before:-bottom-2 before:left-0 before:w-1/3 before:border before:border-zinc-500 relative">
+					Password forgotten
+				</h1>
+
 				<PasswordForgottenForm />
+
+				<BackToLoginLink />
 			</div>
 		</div>
 	)
