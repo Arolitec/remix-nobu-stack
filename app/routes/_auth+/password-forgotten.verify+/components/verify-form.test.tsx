3290d57741533006550004fda2e6563285f7daad
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-06 22:16:26 +0000

    Fix validation error in VerifyForm component

--- app/routes/_auth+/password-forgotten.verify+/components/verify-form.test.tsx
+++ app/routes/_auth+/password-forgotten.verify+/components/verify-form.test.tsx
@@ -44,5 +44,15 @@ describe('VerifyForm', () => {
 			screen.getByText(/you must enter a valid email address/i),
 		).toBeInTheDocument()
 		expect(screen.getByText(/invalid otp/i)).toBeInTheDocument()
+
+		await user.type(screen.getByLabelText(/otp/i), '12345')
+		await user.click(
+			screen.getByRole('button', { name: /validate your email/i }),
+		)
+
+		expect(screen.queryByText(/invalid otp/i)).not.toBeInTheDocument()
+		expect(
+			screen.getByText(/you must enter a 6 digits otp/i),
+		).toBeInTheDocument()
 	})
 })
