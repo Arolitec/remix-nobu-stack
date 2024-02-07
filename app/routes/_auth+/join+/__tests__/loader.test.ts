31ab5eb2045195b94b25099566b1c904305c2011
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-07 10:22:28 +0000

    Refactor loader.test.ts in join route

--- app/routes/_auth+/join+/__tests__/loader.test.ts
+++ app/routes/_auth+/join+/__tests__/loader.test.ts
@@ -1,46 +1,48 @@
+import { authenticator } from '~/utils/auth.server'
 import { commitSession, getSession } from '~/utils/session.server'
 import loader from '../loader'
-import { authenticator } from '~/utils/auth.server'
+
+const BASE_URL = 'http://test.com/join'
 
 describe.concurrent('[join] loader', () => {
-  it('should return a 200 response if user not logged', async () => {
-    const response = await loader({
-      request: new Request('http://localhost:3000/join'),
-      context: {},
-      params: {},
-    })
+	it('should return a 200 response if user not logged', async () => {
+		const response = await loader({
+			request: new Request(BASE_URL),
+			context: {},
+			params: {},
+		})
 
-    expect(response).toBeInstanceOf(Response)
-  })
+		expect(response).toBeInstanceOf(Response)
+	})
 
-  it('should return an empty object', async () => {
-    const response = await loader({
-      request: new Request('http://test/join'),
-      context: {},
-      params: {},
-    })
+	it('should return an empty object', async () => {
+		const response = await loader({
+			request: new Request(BASE_URL),
+			context: {},
+			params: {},
+		})
 
-    const body = await response.json()
+		const body = await response.json()
 
-    expect(body).toEqual({})
-  })
+		expect(body).toEqual({})
+	})
 
-  it('should redirect is user is already logged', async () => {
-    const session = await getSession('Cookie')
-    session.set(authenticator.sessionKey, { id: '123' })
+	it('should redirect is user is already logged', async () => {
+		const session = await getSession('Cookie')
+		session.set(authenticator.sessionKey, { id: '123' })
 
-    const request = new Request('http://test/join', {
-      headers: {
-        'Cookie': await commitSession(session),
-      }
-    })
+		const request = new Request(BASE_URL, {
+			headers: {
+				Cookie: await commitSession(session),
+			},
+		})
 
-    const response = await loader({
-      context: {},
-      params: {},
-      request
-    })
+		const response = await loader({
+			context: {},
+			params: {},
+			request,
+		})
 
-    expect(response.status).toBe(302)
-  })
+		expect(response.status).toBe(302)
+	})
 })
