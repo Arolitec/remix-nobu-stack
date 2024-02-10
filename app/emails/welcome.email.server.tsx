92dfcf81cc1343fdcf024a20d1bc43c714be4d4f
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-02-10 23:40:26 +0000

    refactor: Add Heading component and update styles in welcome email

--- app/emails/welcome.email.server.tsx
+++ app/emails/welcome.email.server.tsx
@@ -4,6 +4,7 @@ import {
 	Container,
 	Font,
 	Head,
+	Heading,
 	Hr,
 	Html,
 	Preview,
@@ -11,7 +12,6 @@ import {
 	Tailwind,
 	Text,
 } from '@react-email/components'
-import tailwindConfig from '../../tailwind.config'
 
 interface WelcomeEmailProps {
 	username: string
@@ -22,37 +22,42 @@ export default function WelcomeEmail(props: Readonly<WelcomeEmailProps>) {
 
 	return (
 		<Html lang="en" data-theme="light">
+			<Head>
+				<Font
+					fontFamily="Nunito Sans"
+					fallbackFontFamily="Verdana"
+					webFont={{
+						url: 'https://fonts.gstatic.com/s/nunitosans/v15/pe0TMImSLYBIv1o4X1M8ce2xCx3yop4tQpF_MeTm0lfUVwoNnq4CLz0_upHZPYsZ51Q42ptCprt4R-tQKr51pE8.woff2',
+						format: 'woff2',
+					}}
+				/>
+			</Head>
+
 			<Preview>Welcome to Nobu Stack</Preview>
-			<Tailwind config={tailwindConfig}>
-				<Head>
-					<Font fontFamily="Nunito Sans" fallbackFontFamily="Verdana" />
-				</Head>
-				<Body>
-					<Container>
-						<Section className="flex w-full items-center justify-center">
-							<Text className="text-4xl font-bold uppercase">Nobu Stack</Text>
-						</Section>
-						<Section>
-							<Text className="text-base">Hi {username},</Text>
-							<Text className="text-base">
-								Welcome to Nobu Stack. We hope you will like it!
-							</Text>
-						</Section>
-						<Section className="my-4 flex items-center justify-center">
+
+			<Tailwind>
+				<Body className="bg-white my-auto mx-auto px-2">
+					<Container className="border border-zinc-200 border-solid  px-4 text-sm my-10 max-w-96 rounded">
+						<Heading className="text-xl font-semibold text-center my-8">
+							Nobu Stack
+						</Heading>
+						<Text>Hi {username},</Text>
+						<Text>Welcome to Nobu Stack. We hope you will like it!</Text>
+						<Section className="text-center my-8">
 							<Button
 								href="http://localhost:3000/login"
-								className="btn-primary btn "
+								className="bg-zinc-900 text-zinc-50 whitespace-nowrap rounded-md text-sm font-medium h-4 px-4 py-2"
 							>
 								Get started
 							</Button>
 						</Section>
-						<Text className="text-base">
+						<Text>
 							Best,
 							<br />
-							Arolitec Team
+							Nobu Team
 						</Text>
-						<Hr />
-						<Text className="text-base">
+						<Hr className="my-8" />
+						<Text className="text-zinc-400 font-light text-center mb-8">
 							All rights reserved. {new Date().getFullYear()}
 						</Text>
 					</Container>
